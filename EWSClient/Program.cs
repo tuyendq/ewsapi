41f4fb4ce1fb89908cae491b06220403e2438317
Tuyen Dang <tuyendq@gmail.com>
2020-11-06 22:22:28 +0700

    update ewsclient

--- EWSClient/Program.cs
+++ EWSClient/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Security;
 using System.Net;
+using System.Xml.Linq;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EWSClient
 {
@@ -13,7 +19,8 @@ static void Main(string[] args)
       Tracing.WriteLine("EWS sample application started.");
       var isValidEmailAddress = false;
       Console.Write("Enter an email address: ");
-      var emailAddress = Console.ReadLine();
+      // var emailAddress = Console.ReadLine();
+      var emailAddress = "sven@htb.local";
       
         isValidEmailAddress = (emailAddress.Contains("@") && emailAddress.Contains("."));
       if (!isValidEmailAddress)
@@ -27,8 +34,11 @@ static void Main(string[] args)
         Tracing.WriteLine("Password empty, closing program.");
       }
       NetworkCredential userCredentials = new NetworkCredential(emailAddress, password);
+
+      ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
       // These are the sample methods that demonstrate using EWS.
-      // ShowNumberOfMessagesInInbox(userCredentials);
+      ShowNumberOfMessagesInInbox(userCredentials);
       // SendTestEmail(userCredentials);
      
       Tracing.WriteLine("EWS sample application ends.");
@@ -39,6 +49,115 @@ static void Main(string[] args)
     // These method stubs will be filled in later.
     private static void ShowNumberOfMessagesInInbox(NetworkCredential userCredentials)
     {
+/// This is the XML request that is sent to the Exchange server.
+      var getFolderSOAPRequest =
+"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+"<soap:Envelope xmlns:soap=\"https://schemas.xmlsoap.org/soap/envelope/\"\n" +
+"   xmlns:t=\"https://schemas.microsoft.com/exchange/services/2006/types\">\n" +
+"<soap:Header>\n" +
+// "    <t:RequestServerVersion Version=\"Exchange2007_SP1\" />\n" +
+"    <t:RequestServerVersion Version=\"Exchange2010_RTM\" />\n" +
+"  </soap:Header>\n" +
+"  <soap:Body>\n" +
+"    <GetFolder xmlns=\"https://schemas.microsoft.com/exchange/services/2006/messages\"\n" +
+"               xmlns:t=\"https://schemas.microsoft.com/exchange/services/2006/types\">\n" +
+"      <FolderShape>\n" +
+"        <t:BaseShape>Default</t:BaseShape>\n" +
+"      </FolderShape>\n" +
+"      <FolderIds>\n" +
+"        <t:DistinguishedFolderId Id=\"inbox\"/>\n" +
+"      </FolderIds>\n" +
+"    </GetFolder>\n" +
+"  </soap:Body>\n" +
+"</soap:Envelope>\n";
+      // Write the get folder operation request to the console and log file.
+      Tracing.WriteLine("Get folder operation request:");
+      Tracing.WriteLine(getFolderSOAPRequest);
+      // var getFolderRequest = WebRequest.CreateHttp(Office365WebServicesURL);
+      var getFolderRequest = WebRequest.CreateHttp("https://10.10.10.210/EWS/Exchange.asmx");
+      getFolderRequest.AllowAutoRedirect = false;
+      getFolderRequest.Credentials = userCredentials;
+      getFolderRequest.Method = "POST";
+      getFolderRequest.ContentType = "text/xml";
+      var requestWriter = new StreamWriter(getFolderRequest.GetRequestStream());
+      requestWriter.Write(getFolderSOAPRequest);
+      requestWriter.Close();
+      try
+      {
+        var getFolderResponse = (HttpWebResponse)(getFolderRequest.GetResponse());
+        if (getFolderResponse.StatusCode == HttpStatusCode.OK)
+        {
+          var responseStream = getFolderResponse.GetResponseStream();
+          XElement responseEnvelope = XElement.Load(responseStream);
+          if (responseEnvelope != null)
+          {
+            // Write the response to the console and log file.
+            Tracing.WriteLine("Response:");
+            StringBuilder stringBuilder = new StringBuilder();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            XmlWriter writer = XmlWriter.Create(stringBuilder, settings);
+            responseEnvelope.Save(writer);
+            writer.Close();
+            Tracing.WriteLine(stringBuilder.ToString());
+            // Check the response for error codes. If there is an error, throw an application exception.
+            IEnumerable<XElement> errorCodes = from errorCode in responseEnvelope.Descendants
+                                               ("{https://schemas.microsoft.com/exchange/services/2006/messages}ResponseCode")
+                                               select errorCode;
+            foreach (var errorCode in errorCodes)
+            {
+              if (errorCode.Value != "NoError")
+              {
+                switch (errorCode.Parent.Name.LocalName.ToString())
+                {
+                  case "Response":
+                    string responseError = "Response-level error getting inbox information:\n" + errorCode.Value;
+                    throw new ApplicationException(responseError);
+                  case "UserResponse":
+                    string userError = "User-level error getting inbox information:\n" + errorCode.Value;
+                    throw new ApplicationException(userError);
+                }
+              }
+            }
+            // Process the response.
+            IEnumerable<XElement> folders = from folderElement in
+                                              responseEnvelope.Descendants
+                                              ("{https://schemas.microsoft.com/exchange/services/2006/messages}Folders")
+                                            select folderElement;
+            foreach (var folder in folders)
+            {
+              Tracing.Write("Folder name:     ");
+              var folderName = from folderElement in
+                                 folder.Descendants
+                                 ("{https://schemas.microsoft.com/exchange/services/2006/types}DisplayName")
+                               select folderElement.Value;
+              Tracing.WriteLine(folderName.ElementAt(0));
+              Tracing.Write("Total messages:  ");
+              var totalCount = from folderElement in
+                                 folder.Descendants
+                                   ("{https://schemas.microsoft.com/exchange/services/2006/types}TotalCount")
+                               select folderElement.Value;
+              Tracing.WriteLine(totalCount.ElementAt(0));
+              Tracing.Write("Unread messages: ");
+              var unreadCount = from folderElement in
+                                 folder.Descendants
+                                   ("{https://schemas.microsoft.com/exchange/services/2006/types}UnreadCount")
+                               select folderElement.Value;
+              Tracing.WriteLine(unreadCount.ElementAt(0));
+            }
+          }
+        }
+      }
+      catch (WebException ex)
+      {
+        Tracing.WriteLine("Caught Web exception:");
+        Tracing.WriteLine(ex.Message);
+      }
+      catch (ApplicationException ex)
+      {
+        Tracing.WriteLine("Caught application exception:");
+        Tracing.WriteLine(ex.Message);
+      }      
     }
     private static void SendTestEmail(NetworkCredential userCredentials)
     {
